b8d79bf940537664f8a28dda865576c465e39cc3
Jeff Hutchins <zumpiez@gmail.com>
2009-06-13 12:51:54 -0700

    playing with .gitignore bear with me

---
+++ Zeplin/Content/Sprite.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
+
+namespace Zeplin
+{
+    public class Sprite
+    {
+        public Sprite()
+        {
+        }
+
+        public void SetCenter(Vector2 location)
+        {
+
+        }
+        
+        float rotation;
+        Vector2 scale;
+        Texture2D image;
+        Vector2 frameSize;
+    }
+}
