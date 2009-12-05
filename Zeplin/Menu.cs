56e580f0c698a7592b1b8b3a9d1a5169516e30b3
Jeff Hutchins <zumpiez@gmail.com>
2009-12-05 12:41:03 -0800

    Stubbed in Menu and MenuItem classes

---
+++ Zeplin/Menu.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Zeplin
+{
+    class Menu : GameObject
+    {
+        public Menu()
+        {
+            OnUpdate += Update;
+            OnDraw += Draw;
+        }
+
+        /// <summary>
+        /// Adds a MenuItem to the Menu using a default name.
+        /// </summary>
+        /// <param name="item">A MenuItem that will belong to this menu.</param>
+        public void Add(MenuItem item)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Adds a MenuItem to the Menu using a specified name.
+        /// </summary>
+        /// <param name="item">A MenuItem that will belong to this menu.</param>
+        /// <param name="name">The MenuItem can be referenced by this name.</param>
+        public void Add(MenuItem item, String name)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Removes a MenuItem from the Menu by object reference
+        /// </summary>
+        /// <param name="item">A MenuItem that currently belongs to the Menu.</param>
+        public void Remove(MenuItem item)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Removes a MenuItem from the Menu by name
+        /// </summary>
+        /// <param name="name">the name of a MenuItem</param>
+        public void Remove(String name)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Draws all MenuItems associated with the Menu.
+        /// </summary>
+        /// <param name="time"></param>
+        internal void Draw(GameTime time)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Gets keyboard input and modifies MenuItem states.
+        /// </summary>
+        /// <param name="time"></param>
+        internal void Update(GameTime time)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<MenuItem> MenuItems { get; set; }
+
+        KeyValuePair<String, MenuItem> menuItemCollection;
+    }
+}
---
+++ Zeplin/MenuItem.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Zeplin
+{
+    public enum ItemState
+    {
+        active, inactive
+    }
+    
+    public class MenuItem
+    {
+        public MenuItem()
+        {
+        }
+
+        /// <summary>
+        /// Draws the MenuItem.
+        /// </summary>
+        /// <param name="time"></param>
+        internal void Draw(GameTime time)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Gets or sets the state of the MenuItem. This may affect its draw behavior.
+        /// </summary>
+        internal ItemState State { get; set; }
+
+        //public member until struct properties are figured out
+        public Transformation Transformation;
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        /// <summary>
+        /// The default name of the MenuItem, based on the type and hash value
+        /// </summary>
+        internal String DefaultName
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
