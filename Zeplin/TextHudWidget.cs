c777bace8c0cb15f851bada619fc41d620b29251
vogon <vogon@icculus.org>
2009-12-05 20:35:22 -0800

    Added TextHudWidget class

---
+++ Zeplin/TextHudWidget.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Zeplin
+{
+    class TextHudWidget : IGameObjectProvider
+    {
+        private GameObject gameObject = new GameObject();
+        public GameObject GameObject
+        {
+            get { return gameObject; }
+        }
+
+        public TextHudWidget(string text)
+        {
+            Text = text;
+
+            GameObject.OnDraw += this.Draw;
+        }
+
+        private void Draw(GameTime now)
+        {
+
+        }
+
+        public string Text { get; set; }
+    }
+}
