b6adf57d1487b556c986f73dea375f4e8c18e82e
vogon <vogon@icculus.org>
2009-12-11 15:21:30 -0800

    Added DungeonTile.  Renamed chunk to Chunk to comply with .net style.  Dropped a bangin' donk on it.

---
+++ TetrisRogue/DungeonTile.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Zeplin;
+
+namespace TetrisRogue {
+    enum Navigability 
+    {
+        Navigable,
+        Wall
+    }
+
+    struct DungeonTile 
+    {
+        public DungeonTile(Sprite sprite, Rectangle extent, Navigability nav) 
+        {
+            _sprite = sprite;
+            _extent = extent;
+            _navigability = nav;
+        }
+
+        public Sprite Sprite { get { return _sprite; } }
+        public Rectangle Extent { get { return _extent; } }
+        public Navigability Navigability { get { return _navigability; } }
+
+        private readonly Sprite _sprite;
+        private readonly Rectangle _extent;
+        private readonly Navigability _navigability;
+    }
+}
