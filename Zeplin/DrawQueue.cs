8b84cfdfed88f40138ea74e82a668cfc84ac4b7d
Jeff Hutchins <zumpiez@gmail.com>
2010-02-07 18:11:18 -0800

    Further work on DrawQueue

---
+++ Zeplin/DrawQueue.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace Zeplin
+{
+    class DrawQueue
+    {
+        public DrawQueue()
+        {
+            drawQueue = new Queue<Queue<DrawCommand>>();
+            currentDrawLayer = new Queue<DrawCommand>();
+            parallaxQueue = new Queue<Vector2>();
+        }
+        
+        public void AddCommand(DrawCommand cmd)
+        {
+            currentDrawLayer.Enqueue(cmd);
+        }
+
+        public void NextLayer()
+        {
+            NextLayer(Vector2.One);
+        }
+
+        public void NextLayer(Vector2 parallaxFactor)
+        {
+            drawQueue.Enqueue(currentDrawLayer);
+            parallaxQueue.Enqueue(parallaxFactor);
+            currentDrawLayer = new Queue<DrawCommand>();
+        }
+
+        public void Draw()
+        {
+            //finalize the current queue and put it in the structure.
+            drawQueue.Enqueue(currentDrawLayer);
+           
+            //draw all queues
+            while (drawQueue.Count > 0)
+            {
+                currentDrawLayer = drawQueue.Dequeue();
+                var parallaxFactors = parallaxQueue.Dequeue();
+                var lastDestination = currentDrawLayer.Peek().destination; //initialize to the first RenderTarget in this layer's queue
+                
+                var parallaxMatrix = ComputeParallax(lastDestination, parallaxFactors);
+
+                //get the initial draw state and initialize the sprite batch for the current layer's settings.
+                ZeplinGame.GraphicsDeviceManager.GraphicsDevice.SetRenderTarget(0, lastDestination); //this is the first RenderTarget from the first queue.
+                ZeplinGame.spriteBatch.Begin(SpriteBlendMode.AlphaBlend, SpriteSortMode.BackToFront, SaveStateMode.None, parallaxMatrix);
+
+                //draw all commands in the queue for this layer
+                while (currentDrawLayer.Count > 0)
+                {
+                    DrawCommand current = currentDrawLayer.Dequeue();
+                    if (current.destination != lastDestination) //we must swap render targets
+                    {
+                        ZeplinGame.spriteBatch.End(); //stop this batch before changing device settings
+                        ZeplinGame.GraphicsDeviceManager.GraphicsDevice.SetRenderTarget(0, current.destination);
+                        
+                        //recompute parallax
+                        parallaxMatrix = ComputeParallax(lastDestination, parallaxFactors);
+                        
+                        ZeplinGame.spriteBatch.Begin(SpriteBlendMode.AlphaBlend, SpriteSortMode.Deferred, SaveStateMode.SaveState, parallaxMatrix);
+                    }
+                    ZeplinGame.spriteBatch.Draw(current.source, current.transformation.Position, null, Color.White, current.transformation.Rotation, current.transformation.Pivot, current.transformation.Scale, SpriteEffects.None, current.transformation.Depth);
+                    lastDestination = current.destination;
+                }
+                //this layer is done, loop back to dequeue next layer, compute parallax, etc
+            }
+
+            //make sure all the queues are empty
+            System.Diagnostics.Debug.Assert(currentDrawLayer.Count == 0);
+            System.Diagnostics.Debug.Assert(drawQueue.Count == 0);
+            System.Diagnostics.Debug.Assert(parallaxQueue.Count == 0);
+        }
+
+        /// <summary>
+        /// Returns the parallax matrix if rendering to the screen (drawing to layer), or a 1,1 parallax factor matrix if drawing to another image.
+        /// </summary>
+        /// <param name="destination"></param>
+        /// <param name="parallax"></param>
+        /// <returns></returns>
+        Matrix ComputeParallax(RenderTarget2D destination, Vector2 parallax)
+        {
+            if (destination == null)
+                return Engine.Camera.ComputeViewMatrix(parallax);
+            else
+                return Engine.Camera.ComputeViewMatrix(Vector2.One);
+        }
+
+        Queue<DrawCommand> currentDrawLayer;
+        Queue<Queue<DrawCommand>> drawQueue;
+        Queue<Vector2> parallaxQueue;
+    }
+
+    struct DrawCommand
+    {
+        public DrawCommand(Texture2D source, Transformation transformation, RenderTarget2D destination)
+        {
+            this.source = source;
+            this.transformation = transformation;
+            this.destination = destination;
+        }
+
+        public DrawCommand(Texture2D source, Transformation transformation)
+        {
+            this.source = source;
+            this.transformation = transformation;
+            this.destination = null;
+        }
+
+        internal Texture2D source;
+        internal RenderTarget2D destination;
+        internal Transformation transformation;
+    }
+}
---
+++ Zeplin/Image.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Zeplin.Utilities;
+
+namespace Zeplin
+{
+    public class Image : IDisposable
+    {
+        public Image()
+        {
+            ContentLostEventHandler = new EventHandler(RestoreFromCache);
+        }
+
+        public void Load(Texture2D source)
+        {
+            this.texture = source;
+            Width = source.Width;
+            Height = source.Height;
+            renderTarget = GraphicsHelper.CreateRenderTarget(Width, Height);
+        }
+
+        public void Load(Image source)
+        {
+            throw new NotImplementedException();
+            //todo do we want to COPY the texture and rendertarget, or reference them?
+            //what is the best way to copy a texture and render target?
+        }
+
+        public void Load(string resource)
+        {
+            this.texture = ZeplinGame.ResourceContent.Load<Texture2D>(resource);
+            Width = texture.Width;
+            Height = texture.Height;
+            renderTarget = GraphicsHelper.CreateRenderTarget(Width, Height);
+        }
+
+        /// <summary>
+        /// Draws an image into another image.
+        /// </summary>
+        /// <param name="otherImage"></param>
+        public void Draw(Image otherImage)
+        {
+            this.Draw(otherImage, Transformation.Identity);
+        }
+
+        /// <summary>
+        /// Draws an image into another image.
+        /// </summary>
+        /// <param name="otherImage"></param>
+        /// <param name="transformation"></param>
+        public void Draw(Image otherImage, Transformation transformation)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Draws an image to the screen
+        /// </summary>
+        /// <param name="transformation"></param>
+        public void Draw(Transformation transformation)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Draws an image to the screen
+        /// </summary>
+        public void Draw()
+        {
+            this.Draw(Transformation.Identity);
+        }
+
+        public void Cache()
+        {
+#if !XBOX360
+            Color[] textureData = new Color[texture.Width * texture.Height];
+            texture.GetData<Color>(textureData);
+            cache.SetData<Color>(textureData);
+            renderTarget.ContentLost += ContentLostEventHandler;
+#endif
+        }
+
+        public void RestoreFromCache(Object sender, EventArgs e)
+        {
+#if !XBOX360
+            if (cache != null)
+            {
+                Color[] textureData = new Color[Width * Height];
+                cache.SetData<Color>(textureData);
+                texture.GetData<Color>(textureData);
+            }
+            if (cache == null)
+                renderTarget.ContentLost -= ContentLostEventHandler;
+#endif
+        }
+
+        internal void FlushCache()
+        {
+            if (cache != null)
+            {
+                cache.Dispose();
+                cache = null;
+            }
+        }
+
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+
+
+        internal Texture2D texture;
+        internal RenderTarget2D renderTarget;
+        internal Texture2D cache;
+
+        bool XNASafe; //todo do something with this to make sure that we don't lost textures that XNA should
+                      //be able to fix for us.
+
+        EventHandler ContentLostEventHandler;
+
+        #region IDisposable Members
+
+        public void Dispose()
+        {
+            texture.Dispose();
+            renderTarget.Dispose();
+            cache.Dispose();
+        }
+
+        #endregion
+    }
+}
